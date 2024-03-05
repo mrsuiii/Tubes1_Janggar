3beded1db0b9d3809bda83bb6115dbd72e1240c3
mrsuiii <emeryfathanzwageri@gmail.com>
2024-03-05 22:11:10 +0700

    feat : add closest diamond to bot function

--- game/util.py
+++ game/util.py
@@ -1,5 +1,5 @@
 from .models import Position
-
+from .models import GameObject,Board
 
 def clamp(n, smallest, largest):
     return max(smallest, min(n, largest))
@@ -15,3 +15,24 @@ def get_direction(current_x, current_y, dest_x, dest_y):
 
 def position_equals(a: Position, b: Position):
     return a.x == b.x and a.y == b.y
+
+
+def closest_diamond(bot : GameObject , board : Board):
+    list_diamond  = board.diamonds
+    closest = 10000000
+    best_dia  =  None
+    
+    for dia in list_diamond:
+        x_len = abs(dia.position.x-bot.position.x)
+        y_len = abs(dia.position.x-bot.position.y)
+        dist = x_len + y_len
+        if best_dia == None:
+            closest = dist
+            best_dia = dia
+        else:
+            if closest > dist:
+                closest = dist
+                best_dia = dia
+    #mereturn posisi diamon terdekat dengan bot
+    return best_dia.position
+
