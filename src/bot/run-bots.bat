fd3dfd032eea6ce9ed6c241d1d463ed9f689c74c
RayNoor0 <rayendraalthaf04@gmail.com>
2024-03-09 23:27:35 +0700

    delete folder

--- src/bot/run-bots.bat
+++ src/bot/run-bots.bat
@@ -1,5 +1,5 @@
 @echo off
-@REM start cmd /c "python main.py --logic Nearest --email=test@email.com --name=stima --password=123456 --team etimo"
-@REM start cmd /c "python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Nearest --email=test@email.com --name=stima --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo"
 start cmd /c "python main.py --logic Begal --email=test2@email.com --name=stima2 --password=123456 --team etimo"
 start cmd /c "python main.py --logic Damai --email=test3@email.com --name=stima3 --password=123456 --team etimo"
